e2998056379deaa45c5fa2e043341fe7bbff5b83
Rion <rion.it456@gmail.com>
2023-02-14 00:05:08 +0900

    add customer page and more, fixed layouts

---
+++ src/pages/customer.js
@@ -0,0 +1,82 @@
+import Header from "components/Header";
+import { Layout } from "components/Layout";
+import { useRouter } from "next/router";
+import styles from "src/styles/customer.module.css";
+
+export default function Customer() {
+  const router = useRouter();
+  const toAddCustomer = () => {
+    router.push({pathname: "/customer/addcustomer"}, "/customer/addcustomer")
+  }
+  return (
+    <Layout>
+      <Header user={router.query.loginId} />
+      <div className={styles.container}>
+        <div className={styles.searchForm}>
+          <input type="text" placeholder="顧客検索" />
+          <button className={styles.searchBtn}>検索</button>
+        </div>
+        <div className={styles.customerList}>
+          {/* <h2>顧客がいません</h2> */}
+          <div className={styles.listHeader}>
+            <div>顧客番号</div>
+            <div>氏名</div>
+            <div>登録日</div>
+            <div>最近の宿泊日</div>
+          </div>
+          <ul>
+            <li>
+              <div>JP202302000011</div>
+              <div>山田里温</div>
+              <div>2023/02/12</div>
+              <div>2023/02/12</div>
+              <button>更新</button>
+              <button>削除</button>
+            </li>
+            <li>
+              <div>JP202302000011</div>
+              <div>山田里温</div>
+              <div>2023/02/12</div>
+              <div>2023/02/12</div>
+              <button>更新</button>
+              <button>削除</button>
+            </li>
+            <li>
+              <div>JP202302000011</div>
+              <div>山田里温</div>
+              <div>2023/02/12</div>
+              <div>2023/02/12</div>
+              <button>更新</button>
+              <button>削除</button>
+            </li>
+            <li>
+              <div>JP202302000011</div>
+              <div>山田里温</div>
+              <div>2023/02/12</div>
+              <div>2023/02/12</div>
+              <button>更新</button>
+              <button>削除</button>
+            </li>
+            <li>
+              <div>JP202302000011</div>
+              <div>山田里温</div>
+              <div>2023/02/12</div>
+              <div>2023/02/12</div>
+              <button>更新</button>
+              <button>削除</button>
+            </li>
+            <li>
+              <div>JP202302000011</div>
+              <div>山田里温</div>
+              <div>2023/02/12</div>
+              <div>2023/02/12</div>
+              <button>更新</button>
+              <button>削除</button>
+            </li>
+          </ul>
+        </div>
+        <button className={styles.addCustomer} onClick={toAddCustomer}>顧客登録</button>
+      </div>
+    </Layout>
+  );
+}
--- src/pages/customer/addcustomer.js
+++ src/pages/customer/addcustomer.js
@@ -1,15 +1,15 @@
 import Header from "components/Header";
 import { Layout } from "components/Layout";
 import { useRouter } from "next/router";
-import styles from "src/styles/customer.module.css"
+import styles from "src/styles/addcustomer.module.css"
 
-export default function Customer() {
+export default function AddCustomer() {
   const router = useRouter()
   return (
     <Layout>
       <Header user={router.query.loginId}/>
       <div>
-        hi, this is customer page
+        hi, this is addcustomer page
       </div>
     </Layout>
   );
