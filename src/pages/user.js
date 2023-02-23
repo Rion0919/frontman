81416a6239b0dd8ef9bd8721c2e4260a2800f76e
Rion <rion.it456@gmail.com>
2023-02-23 23:54:13 +0900

    add user page

--- src/pages/user.js
+++ src/pages/user.js
@@ -3,38 +3,89 @@ import { Layout } from "components/Layout";
 import { useRouter } from "next/router";
 import styles from "src/styles/user.module.css";
 import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { auth } from 'src/pages/api/firebase'
+import db, { auth } from 'src/pages/api/firebase'
 import { useEffect, useState } from "react";
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
+import { collection, getDocs } from "firebase/firestore";
 
 export default function User() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const router = useRouter();
-  const handleRegister = async () => {
-    try{
-      await createUserWithEmailAndPassword(auth, email, password)
-      console.log("success");
-    } catch (e) {
-      console.log("--register error--");
-      console.log(e);
+  const router = useRouter()
+  const [users, setUsers] = useState([])
+  const [deleteClick, setDeleteClick] = useState(false);
+  const [userInfo, setUserInfo] = useState({id: null, name: ""});
+  const [deleted, setDeleted] = useState(false);
+  const [words, setWords] = useState('')
+
+  useEffect(() => {
+    setDeleted(false)
+    const dataAry = []
+    const fetch = async () => {
+      const docRef = await getDocs(collection(db, "users"));
+      docRef.forEach((d) => {
+        dataAry.push({
+          id: d.id,
+          name: d.data().name,
+          permission: d.data().permission,
+        })
+      })
+      setUsers(dataAry)
     }
-  }
+    fetch()
+  }, [])
+  // const [email, setEmail] = useState('')
+  // const [password, setPassword] = useState('')
+  // const router = useRouter();
+  // const handleRegister = async () => {
+  //   try{
+  //     await createUserWithEmailAndPassword(auth, email, password)
+  //     console.log("success");
+  //   } catch (e) {
+  //     console.log("--register error--");
+  //     console.log(e);
+  //   }
+  // }
   
   return (
     <Layout>
       <Header user={router.query.loginId} />
-      <input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={handleRegister} >登録</button>
+      <div className={styles.container}>
+        <div className={styles.searchForm}>
+          <input type="text" placeholder="ユーザー検索" value={words} onChange={(e) => setWords(e.target.value)} />
+          <button onClick={() => onClickSearch(words)} className={styles.searchBtn}>検索</button>
+        </div>
+        <div className={styles.customerList}>
+          <div className={styles.listHeader}>
+            <div>ユーザー番号</div>
+            <div>氏名</div>
+            <div>登録日</div>
+            <div>権利</div>
+          </div>
+          <ul>
+            {users.length !== 0 ? (
+              users.map((user, i) => 
+                (
+                <li key={i}>
+                  <div className={styles.customerId}>{user.id}</div>
+                  <div className={styles.customerName}>{user.name}</div>
+                  <div>test</div>
+                  <div>{user.permission}</div>
+                  <button value={user.id}>
+                    更新
+                  </button>
+                  <button onClick={() => deleteCustomer(customer.id)}>
+                    削除
+                  </button>
+                </li>
+              )
+            )
+            ) : (
+              <li>顧客がいません</li>
+            )}
+          </ul>
+        </div>
+        <button className={styles.addCustomer}>
+          顧客登録
+        </button>
+      </div>
     </Layout>
   );
 }
